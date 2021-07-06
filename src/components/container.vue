964514339b2d1a83f58dc809d25ad09faf5529f6
Namjun Kim <wrasf175@naver.com>
2021-07-06 21:36:09 +0900

    Databinding

---
+++ src/components/container.vue
@@ -0,0 +1,35 @@
+<template>
+ <div>
+     <Post :posting="posting[0]"/>
+    <Post :posting="posting[1]"/>
+    <Post :posting="posting[2]"/>
+</div>
+</template>
+
+<script>
+import Post from "./Post.vue"
+
+export default {
+    name : 'container',
+    data(){
+        return {
+
+        }
+    },
+    components : {
+        Post,
+    },
+
+    props : {
+        posting : Array,
+    },
+    method : {
+
+    },
+
+}
+</script>
+
+<style>
+
+</style>
